7401088b448e0983e92209439192e11646ee51ad
Nazar Kosiv <nkosiv@newfireglobal.com>
2022-02-07 16:03:12 +0200

    test change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with me
         </a>
       </header>
       <main>

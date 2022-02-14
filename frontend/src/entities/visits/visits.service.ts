d66527d230f049c95b8a28daaaa0c385e1ff54e8
Nazar Kosiv <nkosiv@newfireglobal.com>
2022-02-14 18:44:59 +0200

    added new build configuration

--- frontend/src/entities/visits/visits.service.ts
+++ frontend/src/entities/visits/visits.service.ts
@@ -1,9 +1,7 @@
-const hostUrl = process.env.REACT_APP_API_HOST as string;
-
 export const getVisits = async () => {
-    return await fetch(`${hostUrl}/api/visits`).then(r => r.json());
+    return await fetch('/api/visits').then(r => r.json());
 };
 
 export const incrementVisits = async () => {
-    return await fetch(`${hostUrl}/api/visits`, { method: 'PUT' });
+    return await fetch('/api/visits', { method: 'PUT' });
 };
